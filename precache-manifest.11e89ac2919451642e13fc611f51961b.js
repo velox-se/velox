c6012fe4ed211b9b02c7d097ec04c7455d4158c0
Ernest <ernestperkowski17@gmail.com>
2020-10-08 18:22:26 +0200

    Updates

---
+++ precache-manifest.11e89ac2919451642e13fc611f51961b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c601b9e3f7bceb2116fc6c384a973b7e",
+    "url": "/velox/index.html"
+  },
+  {
+    "revision": "922ea7e41b370d021683",
+    "url": "/velox/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "0fce631ee014216e02d8",
+    "url": "/velox/static/css/main.7e05e3ff.chunk.css"
+  },
+  {
+    "revision": "922ea7e41b370d021683",
+    "url": "/velox/static/js/2.3f5c7986.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/velox/static/js/2.3f5c7986.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fce631ee014216e02d8",
+    "url": "/velox/static/js/main.c987660b.chunk.js"
+  },
+  {
+    "revision": "95f391958c31f26cb46e",
+    "url": "/velox/static/js/runtime-main.336f0755.js"
+  }
+]);
